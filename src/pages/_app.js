f86ed8d7b73fca50b333cd31c717f7a1ee1b1f20
muturierick <kmuturierick@gmail.com>
2023-05-06 19:20:08 +0300

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import React from 'react';
+import {useEffect, useState} from 'react';
+import { Layout } from '@/components';
+
+import '@/styles/globals.scss'
+import 'tailwindcss/tailwind.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+       <Component {...pageProps} />
+    </Layout>
+  ) 
+  
 }
